03a046c5b45f8cd82bfe88b7a24699cbd8232fa8
Aravind1704003 <aravind1704003@gmail.com>
2024-09-11 21:42:56 +0530

    included ENV

--- server.js
+++ server.js
@@ -1,200 +1,183 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
-
-
-// CORS middleware (allowing all origins for development)
+const apiKey = process.env.API_KEY;
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.post('/nakshatra-durations', async (req, res) => {
-  const apiEndpoint = 'https://json.freeastrologyapi.com/nakshatra-durations';
-  const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j';
+app.post("/nakshatra-durations", async (req, res) => {
+  
 
   try {
-    const response = await axios.post(apiEndpoint, req.body, {
+    const response = await axios.post(process.env.NAKSHATRA_API_END_POINT, req.body, {
       headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': apiKey,
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
       },
     });
 
     res.json(response.data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
-app.post('/yoga-durations', async (req, res) => {
-  const apiEndpoint = 'https://json.freeastrologyapi.com/yoga-durations';
-  const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
-
+app.post("/yoga-durations", async (req, res) => {
   try {
-    const response = await axios.post(apiEndpoint, req.body, {
+    const response = await axios.post(process.env.YOGA_API_END_POINT, req.body, {
       headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': apiKey,
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
       },
     });
 
     res.json(response.data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
-app.post('/tithi-durations', async (req, res) => {
-  console.log('Request Payload:', req.body);
-
-    const apiEndpoint = 'https://json.freeastrologyapi.com/tithi-durations';
-    const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
-  
-    try {
-      const response = await axios.post(apiEndpoint, req.body, {
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': apiKey,
-        },
-      });
-  
-      res.json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  app.post('/karana-durations', async (req, res) => {
-    console.log('Request Payload:', req.body);
-  
-      const apiEndpoint = 'https://json.freeastrologyapi.com/karana-durations';
-      const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
-    
-      try {
-        const response = await axios.post(apiEndpoint, req.body, {
-          headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': apiKey,
-          },
-        });
-    
-        res.json(response.data);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
+app.post("/tithi-durations", async (req, res) => {
+  console.log("Request Payload:", req.body);
+  try {
+    const response = await axios.post(process.env.THITHI_API_END_POINT, req.body, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+      times,
     });
-    app.post('/aayanam', async (req, res) => {
-      console.log('Request Payload:', req.body);
-    
-        const apiEndpoint = 'https://json.freeastrologyapi.com/aayanam';
-        const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/karana-durations", async (req, res) => {
+
+  try {
+    const response = await axios.post(process.env.KARANA_API_END_POINT, req.body, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/aayanam", async (req, res) => {
+  console.log("Request Payload:", req.body);
+
+ 
+
+  try {
+    const response = await axios.post(process.env.AAYANAM_API_END_POINT, req.body, {
       
-        try {
-          const response = await axios.post(apiEndpoint, req.body, {
-            headers: {
-              'Content-Type': 'application/json',
-              'x-api-key': apiKey,
-            },
-          });
-      
-          res.json(response.data);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ error: 'Internal Server Error' });
-        }
-      });
-      app.post('/good-bad-times', async (req, res) => {
-        console.log('Request Payload:', req.body);
-      
-          const apiEndpoint = 'https://json.freeastrologyapi.com/good-bad-times';
-          const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
-        
-          try {
-            const response = await axios.post(apiEndpoint, req.body, {
-              headers: {
-                'Content-Type': 'application/json',
-                'x-api-key': apiKey,
-              },
-            });
-        
-            res.json(response.data);
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
-          }
-        });
-        app.post('/rahu-kalam', async (req, res) => {
-          console.log('Request Payload:', req.body);
-        
-            const apiEndpoint = 'https://json.freeastrologyapi.com/rahu-kalam';
-            const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
-          
-            try {
-              const response = await axios.post(apiEndpoint, req.body, {
-                headers: {
-                  'Content-Type': 'application/json',
-                  'x-api-key': apiKey,
-                },
-              });
-          
-              res.json(response.data);
-            } catch (error) {
-              console.error(error);
-              res.status(500).json({ error: 'Internal Server Error' });
-            }
-          });
-          app.post('/yama-gandam', async (req, res) => {
-            console.log('Request Payload:', req.body);
-          
-              const apiEndpoint = 'https://json.freeastrologyapi.com/yama-gandam';
-              const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
-            
-              try {
-                const response = await axios.post(apiEndpoint, req.body, {
-                  headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': apiKey,
-                  },
-                });
-            
-                res.json(response.data);
-              } catch (error) {
-                console.error(error);
-                res.status(500).json({ error: 'Internal Server Error' });
-              }
-            });
-            app.post('/gulika-kalam', async (req, res) => {
-              console.log('Request Payload:', req.body);
-            
-                const apiEndpoint = 'https://json.freeastrologyapi.com/gulika-kalam';
-                const apiKey = 'dmqJwjSdZ35G8yZjEhK7raBf6nkbg2Oi4AW5EI9j'; 
-              
-                try {
-                  const response = await axios.post(apiEndpoint, req.body, {
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'x-api-key': apiKey,
-                    },
-                  });
-              
-                  res.json(response.data);
-                } catch (error) {
-                  console.error(error);
-                  res.status(500).json({ error: 'Internal Server Error' });
-                }
-              });
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    });
+    console.log(process.env.AAYANAM_API_END_POINT)
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/good-bad-", async (req, res) => {
+  console.log("Request Payload:", req.body);
+
+  
+  try {
+    const response = await axios.post(process.env.GOOD_BAD_API_END_POINT, req.body, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/rahu-kalam", async (req, res) => {
+  console.log("Request Payload:", req.body);
+
+
+
+  try {
+    const response = await axios.post(process.env.RAHU_KALAM_API_END_POINT, req.body, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/yama-gandam", async (req, res) => {
+  console.log("Request Payload:", req.body);
+
+
+
+  try {
+    const response = await axios.post(process.env.YAMA_KANDAM_API_END_POINT, req.body, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/gulika-kalam", async (req, res) => {
+  console.log("Request Payload:", req.body);
+
+
+
+  try {
+    const response = await axios.post(process.env.GULIKA_API_END_POINT, req.body, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
